1bf348fb1ca72f9f317db33db17b2b1c0fbd5477
hyeopsang <dbtkdguq3@gmail.com>
2024-10-20 19:36:32 +0900

    "congif patch"

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-export const REDIRECT_URI = 'https://caferoad.vercel.app//auth/kakao/callback';
+export const REDIRECT_URI = 'https://caferoad.vercel.app/auth/kakao/callback';
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 export const CLIENT_SECRET = process.env.REACT_APP_KAKAO_SECRET_KEY;
