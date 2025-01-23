6fed36957844f96685da2befdea10f07fe1d37db
hyeopsang <dbtkdguq3@gmail.com>
2025-01-23 15:49:02 +0900

    index css 수정

--- src/page/KakaoMap.jsx
+++ src/page/KakaoMap.jsx
@@ -178,7 +178,7 @@ function KakaoMap() {
   };
 
   return (
-    <div className="relative mx-auto min-w-[375px] max-w-[428px] h-svh overflow-hidden">
+    <div className="relative mx-auto min-w-[375px] max-w-[428px] overflow-hidden">
       {menu && <Menu onMenu={setMenu} />}
       <div id="map" style={{ width: "100%", height: "100vh" }} />
       <SearchForm
